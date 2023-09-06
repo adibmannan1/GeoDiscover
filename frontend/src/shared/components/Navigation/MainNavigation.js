234f7ad939f4f8e5a5647f2da03acb10f326562b
adibmannan1 <adibmannan1@gmail.com>
2023-09-06 23:20:18 +0600

    side drawer added

--- frontend/src/shared/components/Navigation/MainNavigation.js
+++ frontend/src/shared/components/Navigation/MainNavigation.js
@@ -1,21 +1,31 @@
 import React from 'react';
 import {Link} from 'react-router-dom';
 
+import SideDrawer from './SideDrawer';
 import MainHeader from './MainHeader';
 import NavLinks from './NavLinks';
 import './MainNavigation.css';
 
 const MainNavigation = props => {
   return (
-    <MainHeader>
+    <>
+      <SideDrawer>
+        <nav className='main-navigation__drawer-nav'>
+          <NavLinks/>
+        </nav>
+      </SideDrawer>
+      <MainHeader>
         <button className='main-navigation__menu-btn'>
-            <span/>
-            <span/>
-            <span/>
+          <span/>
+          <span/>
+          <span/>
         </button>
         <h1 className='main-navigation__title'><Link to='/'>YOUR SPOTS</Link></h1>
-        <nav><NavLinks/></nav>
-    </MainHeader>
+        <nav className='main-navigation__header-nav'>
+          <NavLinks/>
+        </nav>
+      </MainHeader>
+    </>
   )
 }
 
